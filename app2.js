bfba208e5596a0c4ae54ac08782d4c98581a4902
Subrat3066 <{your_github_email}>
2026-03-03 23:00:43 +0530

    added res

---
+++ app2.js
@@ -0,0 +1,12 @@
+let http = require('http');
+let fs = require('fs');
+
+let server = http.createServer((req,res)=>{
+    console.log("this is running");
+    
+    res.write("Backend running successfully");
+    res.end();
+});
+server.listen(4000,()=>{
+    console.log("Server is runnit on port 4000");
+});
